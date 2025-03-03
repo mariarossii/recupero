c7e6b676d9c74add29b93e36d031fb322e5a7469
maria.rossi <maria.rossi@PC1020BIS>
2025-03-03 08:40:07 +0100

    inizio implementazione

---
+++ Recupero/Recupero/Libro.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recupero
+{
+    internal class Libro
+    {
+        private string casa;
+        private int pagine;
+        private string titolo;
+        private List<Partita> partite = new List <Partita>();
+
+        public Libro (string casa, int pagine, string titolo, List<Partita> partite)
+        {
+            this.casa = casa;
+            this.pagine = pagine;
+            this.titolo = titolo;
+            this.partite = partite;
+        }
+
+        public void AggiungiPartita(Partita nuovaPartita)
+        {
+            partite.Add(nuovaPartita);
+        }
+
+        public Partita CercaPerAnno(int anno)
+        {
+            
+        }
+
+    }
+}
