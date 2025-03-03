72322607ca1059a6bb241ba1a39fb164a95e5a0c
maria.rossi <maria.rossi@PC1020BIS>
2025-03-03 08:24:07 +0100

    aggiunta variabili

---
+++ Recupero/Recupero/Partita.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recupero
+{
+    class Partita
+    {
+        private string squadraCasa, squadraFuori;
+        private int anno;
+        private string risultato;
+        private string marcatori;
+
+        public Partita(string squadraCasa, string squadraFuori, int anno, string risultato, string marcatori)
+        {
+            this.squadraCasa = squadraCasa; 
+            this.anno = anno;   
+            this.squadraFuori= squadraFuori;    
+            this.risultato = risultato; 
+            this.marcatori = marcatori;
+        }
+    }
+}
