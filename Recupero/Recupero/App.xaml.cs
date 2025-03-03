ff1aaea2ee4ac4a428ab175f500e298a023a1693
maria.rossi <maria.rossi@PC1020BIS>
2025-03-03 08:15:48 +0100

    inserimento file vs

---
+++ Recupero/Recupero/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Recupero
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
